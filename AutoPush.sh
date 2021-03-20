8746635d53994e71151f397d59a4b79531d8fcbd
Blake_Whitham <bwhitham77@gmail.com>
2021-03-20 16:21:49 -0700

    clean up

--- AutoPush.sh
+++ AutoPush.sh
@@ -10,3 +10,6 @@ git checkout feature
 git push origin feature
 
 #55 11 * * 1-5 /bin/bash//Users/blake/Desktop/MVP/PopCorn/AutoPush.sh
+
+# launchctl unload /Library/LaunchDaemons/com.auto-push.plist
+# launchctl load /Library/LaunchDaemons/com.auto-push.plist
--- RD.sh
+++ RD.sh
@@ -2,11 +2,14 @@
 
 info="Committed: $(date)"
 cd /Users/blake/Desktop/MVP/PopCorn
-chmod 777 log.txt
 chmod 777 RD.sh
+chmod 777 log.txt
 echo "$info" >> log.txt
 
 # Ship it
-git checkout feature
+
 git add log.txt
 git commit -m "$info"
+
+#launchctl unload /Library/LaunchDaemons/com.auto-commit.plist
+# launchctl load /Library/LaunchDaemons/com.auto-commit.plist
