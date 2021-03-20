fa3f433a2bea8b13646e3890b8521c2b0680a536
Blake_Whitham <bwhitham77@gmail.com>
2021-03-20 15:46:03 -0700

    clean up

--- AutoPush.sh
+++ AutoPush.sh
@@ -1,9 +1,11 @@
 #!/bin/bash
 cd /Users/blake/Desktop/MVP/PopCorn
-echo "pushing to native branch: $(date)"
+echo "pushing to feature branch: $(date)"
 chmod 777 log.txt
+chmod 777 AutoPush.sh
 
-echo "pushing to native branch: $(date)" >> log.txt
-git push origin native
+
+echo "pushing to feature branch: $(date)" >> log.txt
+git push origin feature
 
 #55 11 * * 1-5 /bin/bash//Users/blake/Desktop/MVP/PopCorn/AutoPush.sh
--- RD.sh
+++ RD.sh
@@ -3,6 +3,7 @@
 info="Commit: $(date)"
 cd /Users/blake/Desktop/MVP/PopCorn
 chmod 777 log.txt
+chmod 777 RD.sh
 echo "$info" >> log.txt
 
 # Ship it
